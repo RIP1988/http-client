c898486200b3e939aa6ca56286b72172fbeb8236
rip1988 <b4rt3k21@gmail.com>
2019-08-03 17:43:28 +0200

    added exercise promise function

--- src/app/http-client/fun/fun.component.ts
+++ src/app/http-client/fun/fun.component.ts
@@ -13,7 +13,30 @@ export class FunComponent implements OnInit {
   }
 
   test() {
-    console.log('it works!');
+    const decider = 1;
+    return new Promise((resolve, reject) => {
+      if (decider === 1) {
+        resolve(5);
+      } else {
+        reject(1);
+      }
+    })
+    .then((response) => {
+      console.log('obtained response: ' + response);
+      return new Promise((resolve) => resolve(response));
+    })
+    .catch((err) => {
+      console.log('error: ' + err);
+      return new Promise((resolve, reject) => {
+        reject(err);
+      });
+    })
+    .then(response => {
+      console.log('obtained second response: ' + response);
+    })
+    .catch(err => {
+      console.log('error2: ' + err);
+    });
   }
 
 }
